bf08ac37ced5376083d42470607514d11fe20d3d
Dexter Lesaca <dexterlesaca@Dexter-2.local>
2016-10-06 10:48:17 -0700

    add browser-sync task

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,66 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const useref = require('gulp-useref');
 const sass = require('gulp-sass');
+const child = require('child_process');
+const gutil = require('gulp-util');
+const browserSync = require('browser-sync').create();
 
-const cssFiles = './src/sass/**/*.?(s)css';
-const jsFiles = './src/js/**/*.js';
+// PATHS
+const config = {
+  dist: './_site',
+  paths: {
+    cssFiles: './_src/_sass/**/*.?(s)css',
+    jsFiles: [
+      '_src/_scripts/jquery-2.1.1.min.js',
+      '_src/_scripts/jquery.fitvids.js',
+      '_src/_scripts/ap-components.min.js',
+      '_src/_scripts/main.js'
+    ]
+  }
+};
 
+//Sass to CSS Task
 gulp.task('css', () => {
-  gulp.src(cssFiles)
+  gulp.src(config.paths.cssFiles)
   .pipe(sass())
   .pipe(concat('main.css'))
-  .pipe(gulp.dest('./assets/css'));
+  .pipe(gulp.dest('./_src/assets/css'));
 });
 
 gulp.task('js', () => {
-  gulp.src(jsFiles)
+  gulp.src(config.paths.jsFiles)
+  .pipe(concat('bundle.min.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('./assets/js'));
+  .pipe(gulp.dest('./_src/assets/js'));
 });
 
-gulp.task('useref', () => {
-  return gulp.src('./_includes/themes/apache/default.html')
-  .pipe(useref())
-  .pipe(gulp.dest('./assets/js'));
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
 });
 
-gulp.task('watch', () => {
-  gulp.watch(cssFiles, ['css']);
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [config.dist + '/**'],
+    port: 3000,
+    server: {
+      baseDir: config.dist
+    }
+  });
+  gulp.watch(config.paths.cssFiles, ['css', 'jekyll']);
+  gulp.watch(config.paths.jsFiles, ['js', 'jekyll']);
 });
 
-gulp.task('default', ['css', 'js', 'watch']);
+gulp.task('default', ['css', 'js', 'jekyll', 'serve']);
