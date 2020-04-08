5cd9b3b3f4c3e146299f7d075d2f3fd5f8da9467
Janardhan Pulivarthi <j143@protonmail.com>
2020-04-08 14:46:02 +0530

    [SYSTEMML-2539][BUILD] Upgrade build configuration
    - 1. Upgrade gulp to 4.0
         Since the `task` api is deprecated,
         fuctions were used, and exported as tasks.
    - 2. Documentation for `gulpfile.js`.
    - 3. Stick to Susy2, since mixins are not supported in `3.0`
    - 2. Upgrade npm dependencies to state-of-art.
    - 3. Commit package-lock.json file to version control.
    
    Closes #66.

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,15 @@ const child = require('child_process');
 const gutil = require('gulp-util');
 const browserSync = require('browser-sync');
 const sourcemaps = require('gulp-sourcemaps');
+const jekyll = process.platform === 'win32' ? 'jekyll.bat':'jekyll';
 
 // PATHS
 const config = {
   dist: './_site',
   paths: {
-    cssFiles: './_src/_sass/**/*.?(s)css',
+    styles: {
+      src: '_src/_sass/**/*.?(s)css'
+    },
     jsFiles: [
       '_src/_scripts/jquery-2.1.1.min.js',
       '_src/_scripts/jquery.fitvids.js',
@@ -28,37 +31,66 @@ const config = {
   }
 };
 
-//Sass to CSS Task
-gulp.task('css', () => {
-  gulp.src(config.paths.cssFiles)
-      .pipe(sourcemaps.init())
-      .pipe(sass({
-        includePaths: ['node_modules/susy/sass']
-      }).on('error', sass.logError))
-      .pipe(concat('main.css'))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./_src/assets/css'));
-});
+/**
+ * provides command: `jekyll build`
+ */
+function jekyllBuild() {
+  return child.spawn( jekyll, ['build'], {stdio: 'inherit'})
+}
 
-gulp.task('js', () => {
-  gulp.src(config.paths.jsFiles)
-      .pipe(concat('bundle.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./_src/assets/js'));
-});
+/**
+ * style() function takes in `inputs` and 
+ * provides the `output` files.
+ * 
+ * inputs: '_src/_sass/'
+ * include files: 'node_modules/susy/sass'
+ * output: '_src/assets/css/main.css'
+ */
+function style() {
+  return gulp.src(config.paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: ['node_modules/susy/sass'],
+      onError: browserSync.notify
+    }))
+    .pipe(concat('main.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./_src/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+}
 
-gulp.task('jekyll:build', (done) => {
-  browserSync.notify(config.messages.jekyllBuild);
-  const jekyll = child.spawn('jekyll', ['build', '--drafts'], {stdio: 'inherit'})
-    .on('close', done);
-});
+/**
+ * js() function takes in `inputs` and 
+ * provides the `output` files.
+ * 
+ * inputs: '_src/_scripts/'
+ * output: '_src/assets/js/bundle.min.js'
+ */
+ function js() {
+   return gulp.src(config.paths.jsFiles)
+     .pipe(concat('bundle.min.js'))
+     .pipe(uglify())
+     .pipe(gulp.dest('./_src/assets/js'));
+ }
 
-gulp.task('jekyll:rebuild', ['css', 'jekyll:build'], () => {
-    browserSync.reload();
-});
-
-gulp.task('browser-sync', ['css', 'jekyll:build'], () => {
-  browserSync({
+/**
+ * This function serves the static files and 
+ * provides the tooling necessary to debugging
+ * generally exposed at
+ * --------------------------------------
+ *          UI: http://localhost:3001
+ * UI External: http://localhost:3001
+ * --------------------------------------
+ * while, the website files will be served at
+ *  --------------------------------------
+ *      Local: http://localhost:3000
+ *   External: http://192.168.1.100:3000
+ * --------------------------------------
+ * 
+ * @param {*} done 
+ */
+function browserSyncServe(done) {
+  browserSync.init({
     server: {
       baseDir: config.dist,
       serveStaticOptions: {
@@ -66,12 +98,33 @@ gulp.task('browser-sync', ['css', 'jekyll:build'], () => {
       }
     }
   })
-});
+  done();
+}
 
-gulp.task('watch', () => {
-  gulp.watch(config.paths.cssFiles, ['css', 'jekyll:build']);
-  gulp.watch(config.paths.jsFiles, ['js', 'jekyll:build']);
-  gulp.watch(config.paths.markupFiles, ['jekyll:build']);
-});
+/**
+ * This function reloads the browser with 
+ * new changes saved
+ * 
+ * @param {*} done 
+ */
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
 
-gulp.task('default', ['browser-sync', 'watch'])
+/**
+ * watch() method takes
+ *   a path string, 
+ *   an array of path strings,
+ *   an array of glob strings as globs
+ * on the filesystem.      
+ */
+
+function watch() {
+  gulp.watch(config.paths.styles.src, style)
+  gulp.watch(config.paths.jsFiles, js)
+  gulp.watch(config.paths.markupFiles,
+    gulp.series(jekyllBuild, browserSyncReload));
+}
+
+gulp.task('default', gulp.series(style, js, jekyllBuild, browserSyncServe, watch))
